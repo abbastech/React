4279a3f57f784a4a56be41014281ccc2b56f9556
ahmed <ahmedabbas@technologist.com>
2018-01-13 10:15:14 +0500

    React Starting

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ahmed Abbas on 1/13/2018.
+ */
+//This method want 2 arguments,, what and where
+//what should be shown
+//Where should be shown
+// ReactDOM.render(what,where);
+
+
+ReactDOM.render(React.createElement('h1',null,'Hello World'), document.getElementById('content'));
